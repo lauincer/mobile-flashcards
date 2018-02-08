7f38d0996e48e0aa8e33d63d9a52a010e577be84
Laura Incer <laura.incer@razorfish.com>
2018-02-07 23:04:48 -0600

    Using <TextInput>

--- components/DeckCreate.js
+++ components/DeckCreate.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react'
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 
 export default class DeckCreate extends Component {
+  state = {
+    deckName: 'name'
+  }
+
+  handleTextChange = (input) => {
+    this.setState({
+      deckName: input
+    })
+  }
+
   render() {
+    const { deckName } = this.state;
+
     return (
       <View>
         <Text>Deck Create</Text>
+        <TextInput
+          value={deckName}
+          style={styles.input}
+          onChangeText={this.handleTextChange}
+        />
       </View>
     )
   }
@@ -16,5 +33,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     borderRadius: 5,
     padding: 10
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: 'teal'
   }
 })
