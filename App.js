c47bcd44e182176288cf28ede29382e781c45bc1
Laura Incer <laura.incer@razorfish.com>
2018-02-13 19:36:40 -0600

    Added react-navigation package and some comments

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import Card from './components/Card'
 import DeckList from './components/DeckList'
 import DeckCreate from './components/DeckCreate'
+// import { StackNavigator } from 'react-navigation'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Ionicons name='ios-pizza' color='blue' size={200} />
+        {/*<Ionicons name='ios-pizza' color='blue' size={200} />
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
         <Card />
-        <DeckCreate />
+        <DeckCreate />*/}
         <DeckList />
       </View>
     );
@@ -25,7 +26,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
---
+++ utils/api.js
@@ -0,0 +1,25 @@
+// To manage your AsyncStorage database, you'll want to create four different helper methods.
+//
+// getDecks: return all of the decks along with their titles, questions, and answers.
+// getDeck: take in a single id argument and return the deck associated with that id.
+// saveDeckTitle: take in a single title argument and add it to the decks.
+// addCardToDeck: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title. 
+
+import { AsyncStorage } from 'react-native'
+// import { CALENDAR_STORAGE_KEY } from './_calendar'
+//
+// export function submitEntry ({ entry, key }) {
+//   return AsyncStorage.mergeItem(CALENDAR_STORAGE_KEY, JSON.stringify({
+//     [key]: entry
+//   }))
+// }
+//
+// export function removeEntry (key) {
+//   return AsyncStorage.getItem(CALENDAR_STORAGE_KEY)
+//     .then((results) => {
+//       const data = JSON.parse(results)
+//       data[key] = undefined
+//       delete data[key]
+//       AsyncStorage.setItem(CALENDAR_STORAGE_KEY, JSON.stringify(data))
+//     })
+// }
